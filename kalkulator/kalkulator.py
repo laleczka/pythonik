423436a485617af9e2df453f41ec00a2d777a25c
laleczka <aga.porowska@gmail.com>
2016-02-07 21:01:33 +0100

    Ostateczna wersja

--- kalkulator/kalkulator.py
+++ kalkulator/kalkulator.py
@@ -2,15 +2,15 @@
 import abc
 import re
 import string
-import traceback
+
 
 class Expression(object):
     __metaclass__ = abc.ABCMeta
     
     @abc.abstractmethod
     def evaluate(self, env, strict = False):
-        pass
-
+        raise NotImplementedError
+    
 class Set(Expression):
     def __init__(self, elements):
         self._elements = elements
@@ -61,97 +61,87 @@ class Constant(Expression):
         return hash(self._value)
     def evaluate(self, env, strict = False):
         return self
-    
-class Sum(Expression):
-    def __init__(self, left, right):
-        self._left = left
-        self._right = right
-    def __repr__(self):
-        return '({0} u {1})'.format(str(self._left),str(self._right))
-    def __eq__(self, other):
-        if isinstance(other, Sum):
-            return (self._left == other._left) and (self._right == other._right)
-        return False
-    def __hash__(self):
-        return hash(self._left) ^ hash(self._right)
-    def evaluate(self, env, strict = False):
-        left = self._left.evaluate(env, strict=strict)
-        right = self._right.evaluate(env, strict=strict)
-        if isinstance(left, Set) and isinstance(right, Set):
-            return Set(left._elements | right._elements)
-        else:
-            return Sum(left, right)
 
-class Intersection(Expression):
-    def __init__(self, left, right):
-        self._left = left
-        self._right = right
-    def __repr__(self):
-        return '({0} n {1})'.format(str(self._left),str(self._right))
-    def __eq__(self, other):
-        if isinstance(other, Intersection):
-            return (self._left == other._left) and (self._right == other._right)
-        return False
-    def __hash__(self):
-        return hash(self._left) ^ hash(self._right)
-    def evaluate(self, env, strict = False):
-        left = self._left.evaluate(env, strict=strict)
-        right = self._right.evaluate(env, strict=strict)
-        if isinstance(left, Set) and isinstance(right, Set):
-            return Set(left._elements & right._elements)
-        else:
-            return Sum(left, right)
+class Operation(Expression):
+    __metaclass__ = abc.ABCMeta
 
-class RelativeComplement(Expression):
+    @abc.abstractproperty
+    def sign(self):
+        raise NotImplementedError
     def __init__(self, left, right):
         self._left = left
         self._right = right
     def __repr__(self):
-        return '({0} \ {1})'.format(str(self._left),str(self._right))
-    def __eq__(self, other):
-        if isinstance(other, RelativeComplement):
-            return (self._left == other._left) and (self._right == other._right)
-        return False
+        return '({0} {1} {2})'.format(str(self._left), self.sign, str(self._right))
     def __hash__(self):
         return hash(self._left) ^ hash(self._right)
+    def __eq__(self, other):
+        if isinstance(other, type(self)):
+            return (self._left == other._left) and (self._right == other._right)
+        return False
+    @abc.abstractmethod
+    def apply_operation(self, left_elements, right_elements):
+        raise NotImplementedError
+    @abc.abstractmethod
+    def apply_op_to_equal_args(self, arg):
+        raise NotImplementedError
+
     def evaluate(self, env, strict = False):
         left = self._left.evaluate(env, strict=strict)
         right = self._right.evaluate(env, strict=strict)
         if isinstance(left, Set) and isinstance(right, Set):
-            return Set(left._elements - right._elements)
+            return self.apply_operation(left._elements, right._elements)
+        elif left == right:
+            return self.apply_op_to_equal_args(left)
         else:
-            return Sum(left, right)
+            return type(self)(left, right)
     
-class CartesianProduct(Expression):
-    def __init__(self, left, right):
-        self._left = left
-        self._right = right
-    def __repr__(self):
-        return '({0} x {1})'.format(str(self._left),str(self._right))
-    def __eq__(self, other):
-        if isinstance(other, CartesianProduct):
-            return (self._left == other._left) and (self._right == other._right)
-        return False
-    def __hash__(self):
-        return hash(self._left) ^ hash(self._right)
-    def evaluate(self, env, strict = False):
-        left = self._left.evaluate(env, strict=strict)
-        right = self._right.evaluate(env, strict=strict)
-        if isinstance(left, Set) and isinstance(right, Set):
-            big_set = set()
-            for l in left._elements:
-                for r in right._elements:
-                    big_set.add(Set({l,r}))
-            return Set(big_set)
-        else:
-            return CartesianProduct(left, right)
-                
+class Sum(Operation):
+    @property
+    def sign(self):
+        return 'u'
+    def apply_op_to_equal_args(self, arg):
+        return arg
+    def apply_operation(self, left_elements, right_elements):
+        return Set(left_elements | right_elements)
+
+class Intersection(Operation):
+    @property
+    def sign(self):
+        return 'n'
+    def apply_op_to_equal_args(self, arg):
+        return arg
+    def apply_operation(self, left_elements, right_elements):
+        return Set(left_elements & right_elements)
+
+class RelativeComplement(Operation):
+    @property
+    def sign(self):
+        return '\\'
+    def apply_op_to_equal_args(self, arg):
+        return Set(set())
+    def apply_operation(self, left_elements, right_elements):
+        return Set(left_elements - right_elements)
+    
+class CartesianProduct(Operation):
+    @property
+    def sign(self):
+        return 'x'
+    def apply_op_to_equal_args(self, arg):
+        return CartesianProduct(arg, arg)
+    def apply_operation(self, left_elements, right_elements):
+        big_set = set()
+        for l in left_elements:
+            for r in right_elements:
+                big_set.add(Set({l,r}))
+        return Set(big_set)
     
 class Tokenizer(object):
     def __init__(self):
         self._tokens = []
             
-    def tokenize(self, text): 
+    def tokenize(self, text):
+        self._tokens = []
         while len(text)>0:
             a = re.match('^([A-Z]+)|(=:)|([0-9]+)', text)
             if a:
@@ -169,23 +159,23 @@ class Tokenizer(object):
             raise Exception('Invalid string: "{0}"'.format(text))            
         return self._tokens
 
-operations = {
-    'u': Sum,
-    'n': Intersection,
-    '\\': RelativeComplement,
-    'x': CartesianProduct
-    }
 
 class Parser(object):
+    operations = {
+        'u': Sum,
+        'n': Intersection,
+        '\\': RelativeComplement,
+        'x': CartesianProduct}
+
     def __init__(self):
         self._stack = []
 
     def try_parse_operation(self, expression):
-        if self._stack and self._stack[-1] in operations:
+        if self._stack and self._stack[-1] in self.operations:
             op = self._stack.pop()
             if not self._stack:
-                raise Exception('Zle wyrazenie1')
-            self._stack.append(operations[op](self._stack.pop(), expression))
+                raise Exception('Wrong expression.')
+            self._stack.append(self.operations[op](self._stack.pop(), expression))
         else:
             self._stack.append(expression)
 
@@ -207,73 +197,71 @@ class Parser(object):
                     set_a = Set(set(values[::-1]))
                     self.try_parse_operation(set_a)
                 else:
-                    raise Exception('Zle wyrazenie3')
+                    raise Exception('Wrong expression.')
             elif i == ')':
                 if len(self._stack)>1 and self._stack[-2] == '(':
                     a = self._stack.pop()
                     self._stack.pop()
                     self.try_parse_operation(a)
                 else:
-                    raise Exception('Zle wyrazenie8')
+                    raise Exception('Wrong expression.')
             else:
-                raise Exception('Zle wyrazenie9')
+                raise Exception('Wrong expression.')
                     
         if len(self._stack) != 1:
             print self._stack
-            raise Exception('Zle wyrazenie4')
+            raise Exception('Incorrect statement.')
+        if not isinstance(self._stack[0], Expression):
+            raise Exception('Incorrect statement.')
         return self._stack[0]
 
     def parse_print_statement(self, tokens):
         if tokens and tokens[0] == '$':
             return self.parse_expression(tokens[1:])
-        raise Exception('Zle wyrazenie wyœwietlania')
+        raise Exception('Incorrect print statement.')
 
     def parse_assignment(self, tokens):
         if len(tokens) > 1 and tokens[1] in ['=', '=:']:
             name = tokens[0]
             return name, self.parse_expression(tokens[2:])
-        raise Exception('Zle wyrazenie przypisania')
+        raise Exception('Incorrect assigment.')
 
 class Calculator(object):
     def __init__(self):
         self._env = {}
+        self._tokenizer = Tokenizer()
+        self._parser = Parser()
         
     def read_string(self, text):
         if re.match(r'^\$\s*[\\{}()A-Zunx 0-9]+$', text):
-            print 'To jest wyœwietlenie'
             try:
-                tokenizer = Tokenizer()
-                tokens = tokenizer.tokenize(text)
-                parser = Parser()
-                expr = parser.parse_print_statement(tokens)
+                tokens = self._tokenizer.tokenize(text)
+                expr = self._parser.parse_print_statement(tokens)
                 print expr.evaluate(self._env)
             except Exception, e:
                 print str(e)                    
         elif re.match(r'^[A-Z]+\s*=\s*[\\{}()A-Zunx 0-9]+', text):
-            print 'To jest przypisanie bez odroczenia'
             try:
-                tokenizer = Tokenizer()
-                tokens = tokenizer.tokenize(text)
-                parser = Parser()
-                name, expr = parser.parse_assignment(tokens)
+                tokens = self._tokenizer.tokenize(text)
+                name, expr = self._parser.parse_assignment(tokens)
                 self._env[name] = expr.evaluate(self._env, True)
             except Exception, e:
                 print str(e)
-                traceback.print_exc()
         elif re.match(r'^[A-Z]+\s*=:\s*[\\{}()A-Zunx 0-9]+', text):
-            print 'To jest przypisanie z odroczeniem'
-            try:
-                tokenizer = Tokenizer()
-                tokens = tokenizer.tokenize(text)
-                parser = Parser()
-                name, expr = parser.parse_assignment(tokens)
+            try:  
+                tokens = self._tokenizer.tokenize(text)
+                name, expr = self._parser.parse_assignment(tokens)
                 self._env[name] = expr
             except Exception, e:
                 print str(e)
         else:
-            print 'To jest nieprawid³owe wyra¿enie'
+            print "Incorrect expression."
 
-cal = Calculator()
-while True:
-    a = raw_input()
-    cal.read_string(a)
+    def calculate(self):
+        while True:
+            a = raw_input()
+            self.read_string(a)
+
+if __name__ == '__main__':
+    cal = Calculator()
+    cal.calculate()
